f159bdfea6fab9f1788c875606811333dc1275ac
AliBaniJaber <alibanijaber1999@gmail.com>
2025-08-09 12:29:37 -0700

    first commit

--- auth_backend/constants.py
+++ auth_backend/constants.py
@@ -4,3 +4,4 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
 SECRET_KEY = os.getenv("SECRET_KEY", "your-access-secret")
 REFRESH_SECRET_KEY = os.getenv("REFRESH_SECRET_KEY", "your-refresh-secret")
+

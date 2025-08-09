4def1cf608884abf46a7ff19d0b3134c21717f2b
AliBaniJaber <alibanijaber1999@gmail.com>
2025-08-09 14:27:17 -0700

    first commit

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="auth_backend",
+    name="auth-backend",  # Use hyphen if you want
     version="0.1",
-    package_dir={"": "src"},  # Tell setuptools your packages are under src/
-    packages=find_packages(where="src"),  # Find packages under src/
+    packages=find_packages(),  # No where="src"
     install_requires=["bcrypt", "PyJWT", "requests"],
     author="Ali Banijaber",
     description="Reusable JWT-based authentication package with Google and Facebook OAuth support",

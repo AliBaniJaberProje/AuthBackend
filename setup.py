7334b620d20ab2b4d328540313f2c9281977aa58
AliBaniJaber <alibanijaber1999@gmail.com>
2025-08-10 14:23:29 -0700

    first commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auth-backend",
-    version="0.1",
+    version="0.1.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
